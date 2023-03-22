22564b348239200717c190f0906c1ddf7320271a
Ana Lia Camargo <naliah.c@gmail.com>
2023-03-21 21:47:07 -0300

    primeiro commit

---
+++ js/main-rate.js
@@ -0,0 +1,20 @@
+let num=0;
+let i=0;
+
+
+function rating(num){
+    for(i=1; i<=num; i++){
+        document.getElementById("star"+i).src="../assets/vetores/star_full.png";
+    }
+    for(i=num+1; i<=5; i++){
+        document.getElementById("star"+i).src="../assets/vetores/star_empty.png";
+    }
+}
+
+function send(){
+    document.getElementById("mensagem").style.display="none";
+    document.getElementById("btn").style.display="none";
+    document.getElementById("sent").style.display="inline-block";
+    document.getElementById("stars").style.display="none";
+}
+
---
+++ js/main.js
@@ -0,0 +1,9 @@
+function page_load(){
+    setTimeout(newpage, 3600);
+}
+
+function newpage(){
+    window.location="telavideo.html";
+}
+
+page_load();
